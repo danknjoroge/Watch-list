a7cc56f3a00931c7eac2eca8e17d6458cacde37c
danknjoroge <dank8505@gmail.com>
2022-02-24 23:18:55 +0300

    make it better

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY=46a4b679d32eb90f160794c57f509734
-export SECRET_KEY= \x1b\xdf\xfaa\xb7jO\x0c\xda]\xde\xfe\x8f\xf7\xc5\x1b\xdf\xfaa\xb7jO\x0c\xda]\xde\xfe\x8f\xf7\xc5
+export MOVIE_API_KEY='46a4b679d32eb90f160794c57f509734'
+export SECRET_KEY= '\x9e\x07\xfby5I\x87\xb7GgX\xadQ\x9b\xe4\xe9\x84_W\xe8_M;\xa9\xd8'
 
 python3.8 manage.py server
